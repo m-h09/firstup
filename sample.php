bcccc107f91a2d33df2815cd2dab28c4a9954219
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-13 06:25:07 +0900

    php3-1

--- sample.php
+++ sample.php
@@ -1,10 +1,25 @@
 <?php
  $tan = [1,2,3];
 
- $age =20;
- $test ="テスト";
- echo $age;
+ $nizi = [
+    ['松下',29,'男性'],
+    ['高田',25,'男性'],
+    ['岡田',25,'女性']
+ ];
+
+ $ren = [
+  'name' => '田中',
+  'age' =>  24,
+  'codelang' => 'HTML'
+ ];
+
+
+ echo $tan[0];
  echo PHP_EOL;
-//  改行コード
- echo $test;
+ echo '私の名前は'.$nizi[0][0].'です。';
+ echo '年齢は'.$nizi[0][1].'歳'.$nizi[0][2].'です。';
+ echo PHP_EOL;
+ echo $ren['name'];
+
 ?>
+
