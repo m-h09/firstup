1432ef7c74f385d76e64a71ce945639c28805caf
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-18 17:30:21 +0900

    php４
    条件分岐

--- sample.php
+++ sample.php
@@ -1,25 +1,14 @@
 <?php
- $tan = [1,2,3];
-
- $nizi = [
-    ['松下',29,'男性'],
-    ['高田',25,'男性'],
-    ['岡田',25,'女性']
- ];
-
- $ren = [
-  'name' => '田中',
-  'age' =>  24,
-  'codelang' => 'HTML'
- ];
-
-
- echo $tan[0];
- echo PHP_EOL;
- echo '私の名前は'.$nizi[0][0].'です。';
- echo '年齢は'.$nizi[0][1].'歳'.$nizi[0][2].'です。';
- echo PHP_EOL;
- echo $ren['name'];
+for ($i = 1; $i < 14; $i++) {
+   if(!($i % 2 )==1) {
+     continue;
+   }
+   
+   
+   echo $i .' ';
+   echo PHP_EOL;
+   
+}
+echo "for_out";
 
 ?>
-
